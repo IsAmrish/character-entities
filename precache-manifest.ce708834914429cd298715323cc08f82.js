c52be27b99ebddd176ad0184126769e8a8272e8e
IsAmrish <IsAmrish@users.noreply.github.com>
2020-09-06 07:39:12 +0000

    deploy: 004177da1b6f42f1962bd6463eb920bee4ef33e7

---
+++ precache-manifest.ce708834914429cd298715323cc08f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9eb21a14c04c4c2c743eeb315e464e7",
+    "url": "/character-entities/index.html"
+  },
+  {
+    "revision": "ea9c6b7c8a616504c5ec",
+    "url": "/character-entities/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6128f8ef0ccebb128c02",
+    "url": "/character-entities/static/js/2.68449918.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/character-entities/static/js/2.68449918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9c6b7c8a616504c5ec",
+    "url": "/character-entities/static/js/main.a514b306.chunk.js"
+  },
+  {
+    "revision": "43ebd45cf78fee6442cc",
+    "url": "/character-entities/static/js/runtime-main.15389484.js"
+  }
+]);
